6d96eaf563f08a788a149d52a14fb102ced2302c
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-04-02 18:01:52 +0530

    chnages

---
+++ MethodOver.java
@@ -0,0 +1,30 @@
+package BASICS;
+
+import java.util.Scanner;
+
+public class MethodOver {
+
+    public static int calculateArea(int s){
+        return s * s;
+    }
+    public static int calculateArea(int l, int b){
+        return l*b;
+    }
+
+    public static int sumArray(int[] a){
+        for(int i =0;i<=5;i++){
+            return 
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int result = calculateArea(n);
+        System.out.println(result);
+        input.close();
+
+    }
+    
+}
---
+++ Multi.java
@@ -0,0 +1,17 @@
+package BASICS;
+
+public class Multi {
+
+    public static void printMyname(){
+        for(int i =1 ;i<=5;i++){
+            System.out.println("YUVARAJ ENKIRA YUVI!!");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        printMyname();
+    }
+    
+}
