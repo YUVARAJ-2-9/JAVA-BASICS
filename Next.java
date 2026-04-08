77943086964b91915a68be653bb0d9aec4741b69
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-04-08 10:21:46 +0530

    changes

---
+++ Next.java
@@ -0,0 +1,26 @@
+package BASICS;
+
+import java.util.Scanner;
+
+public class Next {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value :");
+        String val = input.nextLine();
+        String rev = "";
+
+        for(int i =val.length()-1;i>=0;i--){
+            rev = rev + val.charAt(i);
+        }
+
+        if(val.equals(rev)){
+            System.out.println("It is Palindrome!");
+        }
+        else{
+            System.err.println("Not Palindrome!");
+        }
+
+
+    }
+}
