1c428e621fdf74b84fbec34b72ad73a8b66565fa
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-04-09 18:54:01 +0530

    changes

---
+++ Split.java
@@ -0,0 +1,22 @@
+package BASICS;
+
+import java.util.Scanner;
+
+public class Split {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value:");
+        String val = input.nextLine().trim();
+
+        String [] word = val.split(" ");
+
+        int count = word.length;
+
+        System.out.println("Total Words :" + count);
+
+
+        
+    }
+}
