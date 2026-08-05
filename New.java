e31ca6de18a73566b3175c75e2c7c9f4eb1308a8
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-08-05 22:28:00 +0530

    ch

---
+++ New.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * New
+ */
+public class New {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int n = in.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+               
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
--- Rnum.java
+++ Rnum.java
@@ -8,7 +8,7 @@ public class Rnum {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + "");
+                System.out.print(i + "");
             }
             System.err.println();
         }
