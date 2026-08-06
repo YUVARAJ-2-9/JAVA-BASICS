5eadce248c746394c39bac7d070975cbfed2be00
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-08-06 07:50:12 +0530

    ch

--- Hallow1.java
+++ Hallow1.java
@@ -15,7 +15,7 @@ public class Hallow1 {
                     System.out.print("*");
                 }
                 for (int j = 1; j <= 2*i; j++) {
-                    System.out.print("-");
+                    System.out.print(" ");
                 }
                 for (int j = n; j > i ; j--) {
                     System.out.print("*");
@@ -24,12 +24,12 @@ public class Hallow1 {
             }
         }
         static void Hallow1(int n){
-            for (int i = 0; i <=  n ; i++) {
+            for (int i = 0; i < n ; i++) {
                 for (int j = 0; j <=i  ; j++) {
                     System.out.print("*");
                 }
-                for (int j = 1; j < 2 * (n - i - 1); j++) {
-                    System.out.print("-");
+                for (int j = 1; j < (2*n+1)-(i*2+2); j++) {
+                    System.out.print(" ");
                 }
                 for (int j =0; j <= i ; j++) {
                     System.out.print("*");
