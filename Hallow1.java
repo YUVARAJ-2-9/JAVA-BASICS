a44718562f7a369ef1635d24455427a3e1492c66
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-08-06 07:46:35 +0530

    ch

---
+++ Hallow1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Hallow1 {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int n = in.nextInt();
+        Hallow(n);
+        Hallow1(n);
+        }
+
+        static void Hallow(int n){
+            for (int i = 0; i < n ; i++) {
+                for (int j = n; j > i ; j--) {
+                    System.out.print("*");
+                }
+                for (int j = 1; j <= 2*i; j++) {
+                    System.out.print("-");
+                }
+                for (int j = n; j > i ; j--) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        static void Hallow1(int n){
+            for (int i = 0; i <=  n ; i++) {
+                for (int j = 0; j <=i  ; j++) {
+                    System.out.print("*");
+                }
+                for (int j = 1; j < 2 * (n - i - 1); j++) {
+                    System.out.print("-");
+                }
+                for (int j =0; j <= i ; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
